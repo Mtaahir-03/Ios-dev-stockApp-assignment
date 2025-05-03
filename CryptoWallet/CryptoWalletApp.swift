6acc8d6d2552009e48b402e7932cd1bc03cdf981
wernkeat <173651601+wernkeat@users.noreply.github.com>
2025-05-03 16:48:40 +1000

    Add base files

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by ウェルン on 3/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
